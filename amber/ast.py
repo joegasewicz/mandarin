a58f77edb7bb1f810bc62f2e0f32f6ebdca6dbde
josefgasewicz <joegasewicz@gmail.com>
2021-08-30 16:30:06 +0100

    init

--- amber/ast.py
+++ amber/ast.py
@@ -1,46 +1,71 @@
 from typing import List, Dict
 from collections import namedtuple
 
-Node = namedtuple("Node", ["value", "operator"], defaults=[None, None])
 
+Node = namedtuple(
+    "Node",
+    ["elem_name", "value"],
+    defaults=[None, None],
+)
+NodeLine = namedtuple(
+    "NodeLine",
+    ["index", "elem_line", "nodes", "child"],
+    defaults=[None, None, [], None],
+)
 SEPARATORS = (".", "#", " ")
+ELEMENTS = ("div", "h1")
+
 
 class AST:
-
-    _node_tree = []
+    """
+    - Each item in the template list gets assigned to a NodeLine
+    """
+    node_line_head: List[NodeLine]
 
     def __init__(self, template: List[str]):
         self.template = template
 
-    @property
-    def node_tree(self):
-        return self._node_tree
+    def create_node(self, node_line: NodeLine):
+        elem_line = ""
+        if not node_line:
+            pass
+        elif node_line.elem_line:
+            if ":\n" in node_line.elem_line:
+                elem_line, _ = node_line.elem_line.split(":\n")
+            elif "\n" in node_line.elem_line:
+                elem_line, _ = node_line.elem_line.split("\n")
+            # Check is elem_line is an element or a string
+            if elem_line in ELEMENTS:
+                n = Node(elem_name=elem_line)
+            else:
+                # Must be a string value
+                n = Node(value=elem_line)
+            node_line.nodes.append(n)
+            self.create_node(node_line.child)
+        elif node_line.child:
+            self.create_node(node_line.child)
+        else:
+            pass
 
-    def create_node(self, chunk: str, separator: str):
-        if separator in chunk:
-            nodes = chunk.split(separator)
-            for n in nodes:
-                if n:
-                    node = Node(value=n)
-                    self.node_tree.append(node)
+    def create_node_line(self, parent_node: NodeLine, template, i: int):
+        if len(template):
+            elem_line = template.pop(0)
+            nl = NodeLine(index=i, elem_line=elem_line, nodes=[], child=None)
+            parent_node.child = nl
+            i += 1
+            self.create_node_line(nl, template, i)
 
-    def build_tree(self, prev_node: Dict = None):
-        for t in reversed(self.template):
-            for separator in SEPARATORS:
-                if separator in t:
-                    for elem in t.split(separator):
-                        if elem:
-                            n = Node(value=elem)
-                            node = {
-                                "node": n,
-                                "children": prev_node,
-                            }
-                            self.build_tree(node)
-                else:
-                    pass
-
-
-
+    def build_tree(self):
+        elem_name: str
+        value: str
+        operator: str
+        children: List[Node]
+        # Create NodeLines
+        i = 1
+        n = NodeLine(index=0)
+        self.create_node_line(n, self.template, i)
+        # Process Lines into nodes
+        return n
 
     def walk(self):
         t = self.build_tree()
--- tests/test_ast.py
+++ tests/test_ast.py
@@ -1,5 +1,5 @@
 from amber.core import Core
-from amber.ast import AST, Node
+from amber.ast import AST, Node, NodeLine
 from amber.elements import ELEMENTS
 
 
@@ -9,35 +9,50 @@ class TestAST:
         core = Core(elements=ELEMENTS, template_path="templates")
         core.get_template("index.amber")
         template = core.template_list
-        assert template == ["div.my-class:\n", '    h1 "hello!"\n']
+
+        assert template == ["div:\n", '    "hello"\n']
+
+    def test_create_nodes(self):
+        core = Core(elements=ELEMENTS, template_path="templates")
+        core.get_template("index.amber")
+        template = core.template_list
+
+        assert template == ["div:\n", '    "hello"\n']
+
+        nl = NodeLine(index=0, elem_line=None, nodes=[], child=NodeLine(
+            index=1, elem_line="div:\n", nodes=[], child=NodeLine(
+                index=2, elem_line='    "hello"\n', nodes=[], child=None),
+            ),
+        )
 
         ast = AST(template)
-        ast.build_tree()
-        result = ast.node_tree
-        expected = [
-            {
-                "node": Node(value="div", operator=None),
-                "children": [
-                    {
-                        "node": Node(value="class", operator="="),
-                        "children": [
-                            {
-                                "node": Node(value="my-class", operator=None),
-                                "children": [
-                                    {
-                                        "node": Node(value="h1", operator=None),
-                                        "children": [
-                                            {
-                                                "node": Node(value="hello!", operator=None),
-                                                "children": None,
-                                            }
-                                        ]
-                                    }
-                                ]
-                            }
-                        ]
-                    },
-                ]
-            }
-        ]
-        assert result == expected
+        ast.create_node(nl)
+
+        expected = NodeLine(index=0, elem_line=None, nodes=[], child=NodeLine(
+            index=1, elem_line="div:\n", nodes=[Node(elem_name="div", value=None)], child=NodeLine(
+                index=2, elem_line='    "hello"\n', nodes=[Node(elem_name=None, value='    "hello"')], child=None),
+            ),
+        )
+        assert nl == expected
+
+
+
+
+    def test_create_node_line(self):
+        core = Core(elements=ELEMENTS, template_path="templates")
+        core.get_template("index.amber")
+        template = core.template_list
+
+        assert template == ["div:\n", '    "hello"\n']
+
+        i = 1
+        node_line = NodeLine(index=0)
+        ast = AST(template)
+        ast.create_node_line(node_line, ast.template, i)
+
+        expected = NodeLine(index=0, elem_line=None, nodes=[], child=NodeLine(
+            index=1, elem_line="div:\n", nodes=[], child=NodeLine(
+                index=2, elem_line='    "hello"\n', nodes=[], child=None),
+            ),
+        )
+        assert expected == node_line
